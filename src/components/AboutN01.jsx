6b3b4c1368f13219ece1e80b52adf778f9e393a5
lydiapedrola <76699386+lydiapedrola@users.noreply.github.com>
2026-06-04 11:40:49 +0200

    Update AboutN01.jsx

--- src/components/AboutN01.jsx
+++ src/components/AboutN01.jsx
@@ -20,7 +20,6 @@ const AboutN01 = () => {
                         <p>Implementación de funcionalidades y documentación del proyecto.</p>
                     </li>
 
-
                     <li>
                         <strong>Marco Antonio Fernández Heras</strong>
                         <p>Desarrollo y validación de pruebas.</p>
@@ -34,6 +33,11 @@ const AboutN01 = () => {
                     <li>
                         <strong>Francisco José Saiz Ezquerra de Paz</strong>
                         <p>Gestión del repositorio, coordinación del equipo y validación de pruebas.</p>
+                    </li>
+
+                    <li>
+                        <strong>Alba Duro Cabrera</strong>
+                        <p>Validación de pruebas y documentación del proyecto.</p>
                     </li>
                 </ul>
             </div>
